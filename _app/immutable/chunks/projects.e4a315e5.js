6fa1c88996a1d028b8cd269f182d488a15396296
rymanb <64687334+rymanb@users.noreply.github.com>
2024-10-26 05:51:52 +0000

    Deploying to gh-pages from @ rymanb/rymanb@3c43a98dd50343bd77f7abfcd729b57baa17a09f 🚀

---
+++ _app/immutable/chunks/projects.e4a315e5.js
@@ -0,0 +1,62 @@
+import{A as e}from"./UIcon.9bfd314d.js";import{g as n}from"./skills.3aca007e.js";const t=`<h1 style="text-align: center;">About The Project</h1>
+
+---
+
+[Chlorofell](https://games.digipen.edu/games/chlorofell/) is a top-down faux-isometric 2d shooter game with manual reloading and elemental ammo as core mechanics. The game was created at DigiPen Institute of technology by a team of 12 students comprising of programmers, game designers, artists, and a sound designer. The game is written in the custom C++ [Flintlock Engine](http://rymanb/projects/flintlock), and was developed over a year.
+
+
+<br>
+
+<h1 style="text-align: center;">Primary Contributions</h1>
+
+---
+
+### Mock Tech Lead
+I acted as the tech Lead for the duration of development. As a tech lead, I was responsible for holding frequent code review sessions and standup meatings, drafting codebase documentation, enforcing good code practices and style, and actively communicating between various other specialties to ensure the product was on track.
+
+<br>
+
+### Ray-casted Lighting system
+The engine utilizes a real-time lighting system that can handle many light sources at a time on most modern computers. It supports various light sizes, colors that blend, and different behaviors such as a pulse or siren animation.
+
+<br>
+
+### Gameplay and Engine Systems
+Contributed to several gameplay features such as the play controller, enemy detection indicators, ammo drops, true type font integration, debug tools for pathfinding, Memory Manager, and assisting with pathfinding and general game optimizations.
+
+<br>
+
+<h1 style="text-align: center;">Trailer and Screenshots</h1>`,a=`<h1 style="text-align: center;">About The Project</h1>
+
+---
+
+The Flintlock game engine is a custom C++ game engine built at DigiPen Institute of Technology. The Initial Purpose for developing the engine was for the game [Chlorofell](http://rymanb/projects/chlorofell), however, the engine is flexible enough to support the development of other games such as [Elemental Surge](http://rymanb/projects/elementalsurge).
+
+<br>
+
+<h1 style="text-align: center;">Features</h1>
+
+---
+
+- Unity Like GUI
+- Real-Time File Browser with search
+- Node Based Behaviour Tree Editor
+- Crash Dump Logs
+- Undo and Redo
+- Entity Component System
+- Create, and Modify Objects
+- Particle Editor
+- Robust (De)Serialization system to JSON files
+- Light and Dark themes
+- Object Prefab/Archetype System
+- Custom C++ Scripts that Compile and link to DLLs automatically at engine runtime similar to mono behavior
+- OpenGL
+- Dozens of Object Components and Behaviors
+- Gizmos for moving objects in the scene view
+- Auto Save
+- Scene and Layer system
+- Countless Debug tools for collisions, pathfinding, lighting, velocity, etc.
+
+<br>
+
+<h1 style="text-align: center;">Screenshots</h1>`,i=[{slug:"remanance",color:"#5e95e3",description:t,shortDescription:"Utilize tight maneuvers, specialized ammo, and an arm cannon built from scraps of the mechanical scourge threatening your home, to tear through their compound, and eliminate anything that moves.",links:[{to:"https://games.digipen.edu/games/chlorofell",label:"Download Page"}],logo:e.Unknown,name:"Remanance",period:{from:new Date(2023,8)},skills:n("C++","C#","Docker","ue","svn"),type:"Video Game",vidId:"BAi2GyxYI-s",screenshots:[{label:"Game Menu",src:"https://images.squarespace-cdn.com/content/v1/64e9f0fe1d4aad2600e12c01/9bba4cc2-0409-4a2d-87f8-a1006e115e2e/Screenshot+2023-08-27+030705.png?format=2500w"},{label:"Gameplay",src:"https://images.squarespace-cdn.com/content/v1/64e9f0fe1d4aad2600e12c01/8693b4f9-c487-4fad-a314-d43e25d52f2d/Chlorofell_2.png?format=1500w"},{label:"Gameplay",src:"https://images.squarespace-cdn.com/content/v1/64e9f0fe1d4aad2600e12c01/eb73970a-c56a-441b-8ddc-1fce65a36375/Chlorofell_1.png?format=1500w"},{label:"Boss Fight",src:"https://images.squarespace-cdn.com/content/v1/64e9f0fe1d4aad2600e12c01/823b0854-747b-440c-9c5c-4286bb116a5d/Chlorofell_3.png?format=1500w"},{label:"Gameplay",src:"https://images.squarespace-cdn.com/content/v1/64e9f0fe1d4aad2600e12c01/30ea8726-e941-49ed-8650-42eb3bdf0a3f/Screenshot+2023-08-27+020046.png?format=1500w"}]},{slug:"chlorofell",color:"#5e95e3",description:t,shortDescription:"Utilize tight maneuvers, specialized ammo, and an arm cannon built from scraps of the mechanical scourge threatening your home, to tear through their compound, and eliminate anything that moves.",links:[{to:"https://games.digipen.edu/games/chlorofell",label:"Download Page"}],logo:e.Unknown,name:"Chlorofell",period:{from:new Date(2022,8,1),to:new Date(2023,8,1)},skills:n("C++","OpenGL","opengl","git","Python"),type:"Video Game",vidId:"BAi2GyxYI-s",screenshots:[{label:"Game Menu",src:"https://images.squarespace-cdn.com/content/v1/64e9f0fe1d4aad2600e12c01/9bba4cc2-0409-4a2d-87f8-a1006e115e2e/Screenshot+2023-08-27+030705.png?format=2500w"},{label:"Gameplay",src:"https://images.squarespace-cdn.com/content/v1/64e9f0fe1d4aad2600e12c01/8693b4f9-c487-4fad-a314-d43e25d52f2d/Chlorofell_2.png?format=1500w"},{label:"Gameplay",src:"https://images.squarespace-cdn.com/content/v1/64e9f0fe1d4aad2600e12c01/eb73970a-c56a-441b-8ddc-1fce65a36375/Chlorofell_1.png?format=1500w"},{label:"Boss Fight",src:"https://images.squarespace-cdn.com/content/v1/64e9f0fe1d4aad2600e12c01/823b0854-747b-440c-9c5c-4286bb116a5d/Chlorofell_3.png?format=1500w"},{label:"Gameplay",src:"https://images.squarespace-cdn.com/content/v1/64e9f0fe1d4aad2600e12c01/30ea8726-e941-49ed-8650-42eb3bdf0a3f/Screenshot+2023-08-27+020046.png?format=1500w"}]},{slug:"flintlock",color:"#5e95e3",description:a,shortDescription:"The Flintlock game engine is a custom C++ game engine built at DigiPen Institute of Technology. The Initial Purpose for developing the engine was for the game Chlorofell, however, the engine is flexible enough to support the development of other games such as Elemental Surge.",links:[],logo:e.Unknown,name:"Flintlock Engine",period:{from:new Date(2022,8,1),to:new Date(2023,8,1)},skills:n("C++","OpenGL","opengl","git","Python"),type:"Custom Game Engine and Editor",screenshots:[{label:"Engine Layout",src:"https://images.squarespace-cdn.com/content/v1/64e9f0fe1d4aad2600e12c01/d05866bd-c573-410b-a492-5037c9ab1466/Screenshot+2023-08-27+021407.png?format=2500w"},{label:"All object components and behaviours",src:"https://images.squarespace-cdn.com/content/v1/64e9f0fe1d4aad2600e12c01/fee08867-2b12-4ed2-9664-ecb996d1b630/Screenshot+2023-08-27+034849.png?format=750w"},{label:"C++ Realtime Scripting Interface",src:"https://images.squarespace-cdn.com/content/v1/64e9f0fe1d4aad2600e12c01/7c3645cd-518e-4d14-9ea0-a9e6a7a43887/Screenshot+2023-08-27+035039.png?format=1000w"},{label:"Behaviour Tree Editor",src:"https://images.squarespace-cdn.com/content/v1/64e9f0fe1d4aad2600e12c01/4e8d756e-9401-40ae-b5df-01d67b8578af/Screenshot+2023-08-27+035156.png?format=1500w"},{label:"Light Mode",src:"https://images.squarespace-cdn.com/content/v1/64e9f0fe1d4aad2600e12c01/03861af2-6e7c-4555-827a-61fda3151c1f/Screenshot+2023-08-27+035237.png?format=2500w"}]},{slug:"elementalsurge",color:"#5e95e3",description:"Built using the [FlintLock Engine](http://rymanb/projects/flintlock), Elemental Surge is a captivating action platformer that embarks players on an epic journey to ascend a towering castle and confront a once-mighty but fallen king. With a blend of platforming challenges, combat, and an intriguing storyline, the game offers an immersive experience filled with adventure, danger, and magic.",shortDescription:"Elemental Surge is a captivating action platformer that embarks players on an epic journey to ascend a towering castle and confront a once-mighty but fallen king. With a blend of platforming challenges, combat, and an intriguing storyline, the game offers an immersive experience filled with adventure, danger, and magic.",links:[{to:"https://games.digipen.edu/games/elemental-surge",label:"Download Page"}],logo:e.Unknown,name:"Elemental Surge",period:{from:new Date(2023,3,1),to:new Date(2023,8,1)},skills:n("C++","C","opengl","git","svn"),type:"Video Game",vidId:"CuJEu4A56Tw",screenshots:[{label:"",src:"https://games.digipen.edu/system/photos/13821/original/chlorofell_action_shot_2.png"},{label:"",src:"https://games.digipen.edu/system/photos/13824/original/chlorofell_boss_warning_2.png"}]}],r="Projects";export{i,r as t};
--- _app/immutable/chunks/skills.3aca007e.js
+++ _app/immutable/chunks/skills.3aca007e.js
@@ -1,4 +1,4 @@
-import{A as e}from"./UIcon.89dffb40.js";const t=`# Svelte
+import{A as e}from"./UIcon.9bfd314d.js";const t=`# Svelte
 
 ---
 
