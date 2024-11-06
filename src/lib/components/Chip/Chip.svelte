03e985d77466898316ad4e797a58deb79e248bb8
Ryman Barnett <boneyshark22@gmail.com>
2024-11-06 01:47:16 -0800

    fixed chip colors not being generated correctly

--- src/lib/components/Chip/Chip.svelte
+++ src/lib/components/Chip/Chip.svelte
@@ -8,6 +8,31 @@
 	export let classes = '';
 	export let href = '';
 	export let border = '[var(--border)]';
+	
+	const getTwBorderColor = (color: string): string => {
+		switch (color) {
+			case 'red':
+				return 'border-red';
+			case 'blue':
+				return 'border-blue';
+			case 'green':
+				return 'border-green';
+			case 'yellow':
+				return 'border-yellow';
+			case 'purple':
+				return 'border-purple';
+			case 'pink':
+				return 'border-pink';
+			case 'cyan':
+				return 'border-cyan';
+			case 'orange':
+				return 'border-orange';
+			case 'gray':
+				return 'border-gray';
+			default:
+				return 'border-gray';
+		}
+	};
 
 	$: className = `row-center cursor-pointer py-[5px] px-[15px] m-[2.5px] decoration-none inline-block border-[1px] border-solid border-${border} rounded-[20px] tracking-wider text-[0.9em] text-[var(--tertiary-text)] duration-[150ms] font-light  ${
 		active
