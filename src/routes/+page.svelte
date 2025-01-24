6a17df508f13f81ff15ccbf4d54b359e72be450b
Ryman Barnett <boneyshark22@gmail.com>
2025-01-24 12:29:48 -0800

    revert last change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -58,7 +58,7 @@
 			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill justify-center" viewBox="0 0 16 16">
 				<path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
 			</svg>
-			<h3 class="md:text-center"> {location}</h3>
+			<h3 class="md:text-center px-2"> {location}</h3>
 		</div>
 		<p class="text-[var(--tertiary-text)]  text-center md:text-center text-[1.2em] font-extralight">
 			{description}
