ddf06e8d78705d3af0680a92946889acf98145a7
Ryman Barnett <boneyshark22@gmail.com>
2025-01-24 17:12:16 -0800

    fixed resume link

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -1,3 +1,3 @@
-export const data = 'https://raw.githubusercontent.com/vinodnimbalkar/svelte-pdf/369db2f9edbf5ab8c87184193e1404340729bb3a/public/sample.pdf';
+export const data = 'https://raw.githubusercontent.com/rymanb/rymanb/master/static/resume.pdf';
 
 export const title = 'Resumé';
